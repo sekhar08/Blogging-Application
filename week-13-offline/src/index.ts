0ae6f0cb3e28bf83c94dcb4d3ce731b922d7609b
sekhar08 <sekhargandrotula@gmail.com>
2025-02-12 14:48:14 +0530

    code for signup/signin routes

--- week-13-offline/src/index.ts
+++ week-13-offline/src/index.ts
@@ -1,27 +1,79 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-
+import { decode, verify, sign } from 'hono/jwt'
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
+    JWT_SECRET: string;
   }
 }>()
 
-app.post('/api/v1/user/signup', (c) => {
 
+app.post('/api/v1/user/signup', async(c) => {
+  
+  const body = await c.req.json()
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
 }).$extends(withAccelerate())
 
-  return c.text('Hello Hono!')
+try{
+
+  const payload = await prisma.user.create({
+    data:{
+      username: body.username,
+      password: body.password,
+      name: body.name
+    }
+  })
+  
+  const jwt = await sign({
+    id:payload.id
+  },c.env.JWT_SECRET)
+    return c.text(jwt)
+}
+
+catch(e){
+    c.status(411)
+    return c.text("Invalid req / something went wrong!!")
+}
 })
 
 
-app.post('/api/v1/user/signin', (c) => {
+app.post('/api/v1/user/signin', async (c) => {
   
-  return c.text('Hello Hono!')
+  const body = await c.req.json()
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+}).$extends(withAccelerate())
+
+try{
+
+  const payload = await prisma.user.findUnique({
+    where:{
+      username: body.username,
+      password: body.password,
+    }
+  })
+
+  if(!payload){
+    c.status(403);
+    return c.json({
+      "error": "Invalid credentials"
+    })
+  }
+  
+  const jwt = await sign({
+    id:payload.id
+  },c.env.JWT_SECRET)
+    return c.text(jwt)
+}
+
+catch(e){
+    c.status(411)
+    return c.text("Invalid req / something went wrong!!")
+}
 })
 
 
