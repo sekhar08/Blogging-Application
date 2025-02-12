01f4f08a71ec4209126315bf78576a64220543df
sekhar08 <sekhargandrotula@gmail.com>
2025-02-12 21:24:23 +0530

    migrated zod types to common folder

--- week-13-offline/src/routes/user.ts
+++ week-13-offline/src/routes/user.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { decode, verify, sign } from 'hono/jwt'
-import { signupInput } from "../zod";
+import { signupInput } from "@chandra_sekhar99/medium-common-v1";
 
 
 
